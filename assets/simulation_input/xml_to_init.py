7d9954e96f08d3eb8e7db9b58b32ccb724468067
DylanGuedes <djmgguedes@gmail.com>
2019-01-16 13:51:02 -0200

    update xml to init script
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- assets/simulation_input/xml_to_init.py
+++ assets/simulation_input/xml_to_init.py
@@ -1,17 +1,19 @@
 import xml.etree.ElementTree as ET
+import sys
 
 if __name__ == '__main__':
-    XML_PATH = "./trips_completo.xml"
-    f = open("ex.init", "w+")
+    XML_PATH = "./sao_paulo/trips_completo.xml"
+    f = open("saopaulo_trips.init", "w+")
 
     gen_id = 1
     tree = ET.parse(XML_PATH).getroot()
     for child in tree:
         attrs = child.attrib
-        for v in attrs["count"]:
+        for v in range(0, int(attrs["count"])):
             payload = """
             {{ class_Car, [#{{id => "{}", origin => "{}", destination => "{}", start_time => {}, start_link => "{}" }}] }}.
             """.format(gen_id, attrs["origin"], attrs["destination"], int(attrs["start"]), attrs["link_origin"])
             f.write(payload)
             gen_id += 1
+        sys.stdout.write('.')
     f.close()
