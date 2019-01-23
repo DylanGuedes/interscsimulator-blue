cded2218b6e654b4a2ea07b572f24423bee05c0b
DylanGuedes <djmgguedes@gmail.com>
2019-01-23 19:14:07 +0000

    bump saopaulo trips
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- assets/simulation_input/xml_to_init.py
+++ assets/simulation_input/xml_to_init.py
@@ -3,7 +3,8 @@ import sys
 
 if __name__ == '__main__':
     XML_PATH = "./sao_paulo/trips_completo.xml"
-    f = open("saopaulo_trips.init", "w+")
+    f = open("saopaulo_trips.init", "a+")
+    computing_node = 2
 
     gen_id = 1
     tree = ET.parse(XML_PATH).getroot()
@@ -11,8 +12,8 @@ if __name__ == '__main__':
         attrs = child.attrib
         for v in range(0, int(attrs["count"])):
             payload = """
-            {{ class_Car, [#{{id => "{}", origin => "{}", destination => "{}", start_time => {}, start_link => "{}" }}] }}.
-            """.format(gen_id, attrs["origin"], attrs["destination"], int(attrs["start"]), attrs["link_origin"])
+            {{ class_Car, [#{{id => "{}", origin => "{}", destination => "{}", start_time => {}, start_link => "{}" }}], node{} }}.
+            """.format(gen_id, attrs["origin"], attrs["destination"], int(attrs["start"]), attrs["link_origin"], 1+(v%computing_node))
             f.write(payload)
             gen_id += 1
         sys.stdout.write('.')
