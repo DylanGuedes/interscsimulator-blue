c6fdd11b29cb506386f73be4d5bebbe7cb351131
DylanGuedes <djmgguedes@gmail.com>
2019-03-18 09:19:37 -0300

    nuvemusp deploy
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- src/update_environment_variables.sh
+++ src/update_environment_variables.sh
@@ -1,6 +1,6 @@
-export KAFKA_BUILD_PATH=/home/dylan/sim-diasca-blue/mock-simulators/interscsimulator-blue/deps/brod/_build
+export KAFKA_BUILD_PATH=/home/ubuntu/sim-diasca-blue/mock-simulators/interscsimulator-blue/deps/brod/_build
 export KAFKA_HOST=172.24.0.71
 export KAFKA_PORT=9092
-export AMQP_CLIENT_PATH=/home/dylan/sim-diasca-blue/mock-simulators/interscsimulator-blue/deps/amqp_client
+export AMQP_CLIENT_PATH=/home/ubuntu/sim-diasca-blue/mock-simulators/interscsimulator-blue/deps/amqp_client
 export RABBITMQ_HOST=172.24.0.71
 
