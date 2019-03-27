059b1bbc798ce3ec2e93791b286607cf9e9d30eb
DylanGuedes <djmgguedes@gmail.com>
2019-03-27 16:48:16 -0300

    adds file check util
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- src/interscsimulator_utils.erl
+++ src/interscsimulator_utils.erl
@@ -1,7 +1,7 @@
 -module(interscsimulator_utils).
 
 -export([print_info/1, print_info/2, print_error/1, print_error/2,
-        print_success/1, print_success/2]).
+        print_success/1, print_success/2, validate_file/1]).
 
 print_info(Text) ->
   io:format("~n\033[94m[INFO]\033[0m ~p", [Text]).
@@ -17,3 +17,11 @@ print_success(Text) ->
   io:format("~n\033[32m[INFO]\033[0m ~p", [Text]).
 print_success(Text, Args) ->
   io:format("~n\033[32m[INFO]\033[0m " ++ Text, Args).
+
+validate_file(Path) ->
+  case filelib:is_regular(Path) of
+    true ->
+      ok;
+    false ->
+      throw({"File does not exist.", Path})
+  end.
